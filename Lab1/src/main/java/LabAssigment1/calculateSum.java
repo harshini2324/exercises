93458cc4924f5e45a95134d2fe5e41f50ea57b79
harshini <harshini@LAPTOP-QSRNTDCD>
2021-05-27 19:38:48 +0530

    my Exercise

--- Lab1/src/main/java/LabAssigment1/calculateSum.java
+++ Lab1/src/main/java/LabAssigment1/calculateSum.java
@@ -7,27 +7,28 @@ import java.io.InputStreamReader;
 public class calculateSum {
 	public static void main(String[] args) throws IOException 
 	{
+
 		System.out.print(" Please Enter any Number to Check whether it is Divisible by 3 or 5 : ");
+		System.out.println("\n");
 		System.out.println(calSum());
 	}
 	private static int calSum() throws IOException {
-		
+		int sum=0;
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		String l=br.readLine();
 		int number1=Integer.parseInt(l);
+		for(int i=1;i<=number1;i++) {
 		
-			if((number1 % 3 == 0) || (number1 % 5 == 0))
-			{
-				System.out.println("\n Given number " + number1 + " is Divisible by 3 or 5"); 
-			}
-			else {
-				System.out.println("number is not divisible by 3 or 5");
-			}
+			if((i % 3 == 0) || (i % 5 == 0))
+				sum=sum+i;
+		}
+		
+				System.out.println("sum of the number divisible by 3 or 5"+sum);
 			
-		
-	
-	return number1;
+		//15}
+ return sum;
 	}
 }
+
 		
 
