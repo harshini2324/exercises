5150a537b3d71415a96c66f635377c8f6ea4094e
harshini <harshini@LAPTOP-QSRNTDCD>
2021-05-27 16:31:02 +0530

    exercise

---
+++ Lab1/src/main/java/LabAssigment1/Fibonacci1.java
@@ -0,0 +1,20 @@
+package LabAssigment1;
+
+public class Fibonacci1 {
+
+	static int n1=1,n2=1,n3=0;    
+	 static void printFibonacci(int count){    
+	    if(count>0){    
+	         n3 = n1 + n2;    
+	         n1 = n2;    
+	         n2 = n3;    
+	         System.out.print(" "+n3);   
+	         printFibonacci(count-1);    
+	     }    
+	 }    
+	 public static void main(String args[]){    
+	  int count=15;    
+	  System.out.print(n1+" "+n2);   
+	  printFibonacci(count-2);  
+	 }  
+}
